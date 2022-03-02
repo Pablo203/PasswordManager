5fbb496025e626caac3b54fe16ca6b51a3212a96
sh4d0w <sh4d0w@pop-os.localdomain>
2022-03-02 18:05:33 +0100

    Added simple conosle interface

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-import sys
+import actions
 passwordMatch = False
-masterPass = "test"
-
-def getMasterPassword():
-    masterPasswordInput = input("Please type master password: ")
-    if(masterPass == masterPasswordInput):
-        print("Please come in")
-        return True
-    else:
-        sys.exit("Password given by you is wrong")
 
 
 
 
 while True:
     if(passwordMatch == False):
-        passwordMatch = getMasterPassword()
+        passwordMatch = actions.getMasterPassword()
     else:
-        input("Here we are")
+        print("Welcome \nWhat would you want to do?")
+        print("1 - Add new password")
+        print("Get password:")
+        print("\t2 - By app name")
+        print("\t3 - By URL")
+        print("\t4 - By Username")
+        option = input()
+        try:
+            option = int(option)
+            if(option > 4 or option < 1):
+                print("You gave wrong option\n")
+            elif(option == 1):
+                pass
+            elif(option == 2):
+                pass
+            elif(option == 3):
+                pass
+            elif(option == 4):
+                pass
+        except ValueError:
+            print("You gave wrong option\n")
