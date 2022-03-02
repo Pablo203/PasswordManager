250f9902e4f794181d4a5d312cde7f9702df725f
sh4d0w <sh4d0w@pop-os.localdomain>
2022-03-02 20:23:28 +0100

    Updated password matching

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import sys
 import db
+import pymysql
+import hashlib
 passwordMatch = False
-masterPass = "test"
 
 def getMasterPassword():
     masterPasswordInput = input("Please type master password: ")
-    if(masterPass == masterPasswordInput):
+
+    connection = pymysql.connect(
+            host = 'localhost', 
+            user = 'root',
+            password = '',
+            database = 'passes'
+        )
+    #Hash inputed phrase
+    hashedPass = hashlib.md5(masterPasswordInput.encode())
+    hashedPass = hashedPass.hexdigest()
+    print(hashedPass)
+
+    #Get hash from db
+    mycursor = connection.cursor()
+    query = "SELECT Pass FROM Pass WHERE ID = 1"
+    mycursor.execute(query)
+    result = mycursor.fetchone()
+    dbHash = []
+    for i in result:
+        dbHash.append(i)
+    print(dbHash[0])
+
+    if(hashedPass == dbHash[0]):
         print("Please come in")
         return True
     else:
@@ -29,22 +52,28 @@ while True:
             if(option > 4 or option < 1):
                 print("You gave wrong option\n")
             elif(option == 1):
-                pass
+                app = input("Give app name: ")
+                url = input("Give url name: ")
+                username = input("Give username: ")
+                password = input("Give password: ")
+
+                input("\nPress enter to continue...")
+                database.addPass(app, url, username, password)
 
             elif(option == 2):
                 appName = input("From which app you want password? ")
                 database.getPassByApp(appName)
-                input("\nPress any key to continue...")
+                input("\nPress enter to continue...")
 
             elif(option == 3):
                 url = input("From which URL you want password? ")
                 database.getPassByUrl(url)
-                input("\nPress any key to continue...")
+                input("\nPress enter to continue...")
 
             elif(option == 4):
                 Username = input("From which Username you want password? ")
                 database.getPassByUsername(Username)
-                input("\nPress any key to continue...")
+                input("\nPress enter to continue...")
 
         except ValueError:
             print("You gave wrong option\n")
